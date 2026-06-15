48cd6cf44de84b7269cadfdeb49d35b8dd9de942
Ivan Shamanaev <van.shamanaev@gmail.com>
2026-06-15 14:40:36 +0300

    fix: changed lagraph based algorithms

---
+++ src/algorithms/lagraph_sssp.h
@@ -0,0 +1,25 @@
+/**
+ * @file lagraph_sssp.h
+ * @brief LAGraph SSSP (Delta-Stepping) через LAGr_SingleSourceShortestPath
+ * 
+ */
+
+#ifndef LAGRAPH_SSSP_H
+#define LAGRAPH_SSSP_H
+
+#include "sssp_common.h"
+
+/**
+ * @brief Запуск LAGraph SSSP (Delta-Stepping)
+ * 
+ * @param result Структура результата
+ * @param graph Граф LAGraph
+ * @param source Исходная вершина
+ * @param delta Параметр шага для Delta-Stepping
+ * @return GrB_SUCCESS если успешно
+ */
+
+GrB_Info lagraph_sssp(SSSP_Result *result, LAGraph_Graph graph, 
+                      GrB_Index source, double delta);
+
+#endif /* LAGRAPH_SSSP_H */
